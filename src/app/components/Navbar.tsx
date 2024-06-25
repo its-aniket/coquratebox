aace016c81fbde6d9d59754c3b45b98b60e7f0ea
its-aniket <aniketjadhav770698@gmail.com>
2024-06-25 10:24:50 +0530

    navbar vertical 1

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from "react";
 import Link from "next/link";
-import { NavLinks } from "@/constants";
+import { NavLinks, sociallinks } from "@/constants";
 
 const Navbar = () => {
   const [isMObileMenu, setMobileMenu] = useState(false);
@@ -58,19 +58,70 @@ const Navbar = () => {
           </button> */}
         </div>
       </div>
-
       {isMObileMenu && (
-        <div className="flex flex-col  justify-center items-center h-auto w-full p-4 shadow-2xl">
-          <ul className="lg:flex text-small gap-7">
-            {NavLinks.map((link) => (
-              <Link href={link.href} key={link.key}>
-                <div className="nav-title group transition duration-300 ">
-                  {link.text}
-                  <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-[#00000]"></span>
-                </div>
-              </Link>
-            ))}
-          </ul>
+        <div className="fixed inset-y-0 right-0 w-[300px] bg-white z-50 shadow-lg lg:hidden">
+          <div className="flex flex-col w-full h-full justify-between p-4">
+            {/* Close Button */}
+            <div className="flex justify-end">
+              <button
+                onClick={() => setMobileMenu(!isMObileMenu)}
+                className="text-2xl"
+              >
+                &times;
+              </button>
+            </div>
+            <div className="w-full flex items-center justify-center">
+              <a href="/">
+                <img
+                  className="w-[130px] h-[46px] m-6 "
+                  src="./logo (2).png"
+                  alt="Logo"
+                />
+              </a>
+            </div>
+            {/* Navigation Links */}
+            <div className="h-full items-start mt-5">
+              <ul className="flex flex-col gap-4 text-lg w-full text-right">
+                {NavLinks.map((link) => (
+                  <div className="flex justify-between items-center border-b-[1px]">
+                    <div>
+                      <img
+                        src="./arrow.png"
+                        alt="down button"
+                        className="w-[25px] h-[25px] -rotate-90"
+                      />
+                    </div>
+                    <Link href={link.href} key={link.key}>
+                      <div className="nav-title group transition duration-300  ">
+                        {link.text}
+                        <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-black"></span>
+                      </div>
+                    </Link>
+                  </div>
+                ))}
+              </ul>
+            </div>
+
+            {/* Social Media Icons */}
+            <div className="w-full h-auto flex flex-col justify-between items-center text-center p-5 lg:flex-row">
+              <div className="w-full justify-center sm:w-[50%] h-auto flex gap-2 items-center sm:justify-start">
+                {sociallinks.map((link, index) => (
+                  <div key={index} className="w-[30px] h-[30px]">
+                    <a
+                      href={link.href}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <img src={link.scr} alt={link.alt} />
+                    </a>
+                  </div>
+                ))}
+              </div>
+              <div className="mt-2 w-full text-center ">
+                @ 2024 coquratebox | all right reserved
+              </div>
+            </div>
+          </div>
         </div>
       )}
     </nav>
