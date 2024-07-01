de97404e511600713fe8fce9e38885b574da032a
its-aniket <aniketjadhav770698@gmail.com>
2024-07-01 17:36:58 +0530

    hero banner 2

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -28,12 +28,11 @@ const HeroSection = () => {
         className="flex transition-transform ease-out duration-500"
         style={{ transform: `translateX(-${curr * 100}%)` }}
       >
-        {herobanner.map((link, index) => (
-          <div key={index} className="w-full flex-shrink-0 relative">
+          <div className="w-full flex-shrink-0 relative">
             <div className="absolute inset-0 flex justify-start items-center text-3xl mt-[27px] flex-col text-center lg:w-full lg:text-black lg:justify-start lg:items-start lg:ml-8 lg:text-6xl lg:text-left ">
               <div className="w-full h-auto flex flex-col justify-center mt-12 text-white items-center gap-5 lg:text-left lg:justify-start lg:items-start">
-                <h1 className={`${dangrek.className} w-[400px] lg:w-[800px]`}>{link.title}</h1>
-                <p className={`${quicksand.className}  text-[24px] w-[400px] `}>{link.para}</p>
+                <h1 className={`${dangrek.className} w-[400px] lg:w-[800px]`}>Uniquely yours...</h1>
+                <p className={`${quicksand.className} max-w-[500px] text-[24px] lg:w-[400px] `}>Create Unforgettable Memories with Our Personalized Gifts</p>
               </div>
               <div className="lg:w-full lg:h-full lg:flex lg:justify-center lg:items-end">
                 <button className=" bg-black mt-[8px] pl-[26px] pr-[26px] pt-[5px] pb-[5px]  rounded-[36px] text-[15px] text-white  lg:mb-7 lg:pt-3 lg:pb-3 transition ease-in-out delay-150 hover:shadow-2xl hover:-translate-y-1">
@@ -43,18 +42,70 @@ const HeroSection = () => {
             </div>
             <img
               className="m-0 p-0 w-full h-[600px] block object-cover lg:hidden"
-              src={link.src1}
-              alt={link.alt1}
+              src="./assets/banner1.webp"
+              alt="banner image mobile"
               loading="lazy"
             />
             <img
               className="m-0 p-0 w-full h-[600px] object-cover hidden lg:block"
-              src={link.src2}
-              alt={link.alt2}
+              src="./assets/banner1.webp"
+              alt="banner image desktop"
               loading="lazy"
             />
           </div>
-        ))}
+        {/* banner 2 */}
+        <div className="w-full flex-shrink-0 relative">
+            <div className="absolute inset-0 flex justify-start items-center text-3xl mt-[27px] flex-col text-center lg:w-full lg:text-black lg:justify-start lg:items-start lg:ml-8 lg:text-6xl lg:text-left ">
+              <div className="w-full h-auto flex flex-col justify-center  items-center gap-5 lg:text-left lg:justify-start lg:items-start lg:mt-12 text-white">
+                <h1 className={`${dangrek.className} w-[400px] lg:w-[800px]`}>Crafted for You!</h1>
+                <p className={`${quicksand.className}  text-[24px] lg:w-[400px] `}>Craft the Perfect Present with Our Custom Gift Sets</p>
+              </div>
+              <div className="lg:w-full lg:h-full lg:flex lg:justify-center lg:items-end">
+                <button className=" bg-black mt-[8px] pl-[26px] pr-[26px] pt-[5px] pb-[5px]  rounded-[36px] text-[15px] text-white  lg:mb-7 lg:pt-3 lg:pb-3 transition ease-in-out delay-150 hover:shadow-2xl hover:-translate-y-1">
+                  shop now
+                </button>
+              </div>
+            </div>
+            <img
+              className="m-0 p-0 w-full h-[600px] block object-cover lg:hidden"
+              src="./assets/bannerM2.webp"
+              alt="banner image mobile"
+              loading="lazy"
+            />
+            <img
+              className="m-0 p-0 w-full h-[600px] object-cover hidden lg:block"
+              src="./assets/banner2.webp"
+              alt="banner image desktop"
+              loading="lazy"
+            />
+          </div>
+          {/* banner 3 */}
+          <div className="w-full flex-shrink-0 relative">
+            <div className="absolute inset-0 flex justify-start items-center text-3xl mt-[27px] flex-col text-center lg:w-full lg:text-black lg:justify-start lg:items-start lg:ml-8 lg:text-6xl lg:text-left ">
+              <div className="w-full h-auto flex flex-col justify-center mt-12 text-black items-center gap-5 lg:text-left lg:justify-start lg:items-start">
+                <h1 className={`${dangrek.className} w-[400px] lg:w-[800px]`}>Coporate Elegance!</h1>
+                <p className={`${quicksand.className}  text-[24px] lg:w-[450px] `}>Elevate Your Professional Relationships with Premium Corporate Gifts</p>
+              </div>
+              <div className="lg:w-full lg:h-full lg:flex lg:justify-center lg:items-end">
+                <button className=" bg-black mt-[8px] pl-[26px] pr-[26px] pt-[5px] pb-[5px]  rounded-[36px] text-[15px] text-white  lg:mb-7 lg:pt-3 lg:pb-3 transition ease-in-out delay-150 hover:shadow-2xl hover:-translate-y-1">
+                  shop now
+                </button>
+              </div>
+            </div>
+            <img
+              className="m-0 p-0 w-full h-[600px] block object-cover lg:hidden"
+              src="./assets/bannerM3.webp"
+              alt="banner image mobile"
+              loading="lazy"
+            />
+            <img
+              className="m-0 p-0 w-full h-[600px] object-cover hidden lg:block"
+              src="./assets/banner3.webp"
+              alt="banner image desktop"
+              loading="lazy"
+            />
+          </div>
+
       </div>
       <div className="absolute w-[140px] ml-[70%] mr-3 bottom-[1px] pb-[20px] flex justify-between space-x-4 right-4">
         <button
