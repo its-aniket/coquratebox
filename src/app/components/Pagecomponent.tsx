09c52bbb2993ae2f1195c6c2a04e260e3bbe76a6
its-aniket <aniketjadhav770698@gmail.com>
2024-08-05 13:52:22 +0530

    commit for metadata

---
+++ src/app/components/Pagecomponent.tsx
@@ -0,0 +1,54 @@
+
+"use client"
+import React, { useState, useEffect, useRef } from "react";
+import Navbar from "./Navbar";
+import HeroSection from "./HeroSection";
+import FeaturedCollection from "./FeaturedCollection";
+import CorporateBanner from "./CorporateBanner";
+import PersonalizeBanner from "./PersonalizeBanner";
+import Testimonials from "./Testimonials";
+import ContactUs from "./ContactUs";
+import NotableClient from "./NotableClient";
+import Footer from "./Footer";
+import PopUp from "./PopUp";
+import AnnouncementBar from "./AnnouncementBar";
+const Pagecomponent = () => {
+    const [showPopup, setShowPopup] = useState(false);
+  const contactUsRef = useRef<HTMLDivElement>(null);
+
+  const scrollToSection = () => {
+    if (contactUsRef.current) {
+      contactUsRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowPopup(true);
+    }, 5000); // Show pop-up after 10 seconds
+
+    return () => clearTimeout(timer); // Clear the timer if the component unmounts
+  }, []);
+  return (
+    
+    <main className="p-0 m-0 scroll-smooth">
+      <AnnouncementBar />
+      <Navbar />
+      <HeroSection />
+      <FeaturedCollection />
+      <CorporateBanner scrollToSection={scrollToSection}/>
+      <PersonalizeBanner scrollToSection={scrollToSection} />
+      <Testimonials />
+      <NotableClient />
+      <div ref={contactUsRef}>
+        <ContactUs />
+      </div>
+      <Footer />
+
+      {showPopup && <PopUp onClose={() => setShowPopup(false)}  scrollToSection={scrollToSection}/>}
+    </main>
+    
+  )
+}
+
+export default Pagecomponent
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,74 +1,18 @@
-"use client"
-import React, { useState, useEffect, useRef } from "react";
-import Navbar from "./components/Navbar";
-import HeroSection from "./components/HeroSection";
-import FeaturedCollection from "./components/FeaturedCollection";
-import CorporateBanner from "./components/CorporateBanner";
-import PersonalizeBanner from "./components/PersonalizeBanner";
-import Testimonials from "./components/Testimonials";
-import ContactUs from "./components/ContactUs";
-import NotableClient from "./components/NotableClient";
-import Footer from "./components/Footer";
-import PopUp from "./components/PopUp";
-import AnnouncementBar from "./components/AnnouncementBar";
-import  Head  from "next/head";
+import React from "react";
+import Pagecomponent from "./components/Pagecomponent";
+
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Coqurate Box",
+  description:
+    "Coqurate Box is a corporate and personalized gifting solution company that believes in the power of thoughtful gifts to build connections and inspire loyalty.",
+};
 
 const Home: React.FC = () => {
-  const [showPopup, setShowPopup] = useState(false);
-  const contactUsRef = useRef<HTMLDivElement>(null);
-
-  const scrollToSection = () => {
-    if (contactUsRef.current) {
-      contactUsRef.current.scrollIntoView({ behavior: "smooth" });
-    }
-  };
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setShowPopup(true);
-    }, 5000); // Show pop-up after 10 seconds
-
-    return () => clearTimeout(timer); // Clear the timer if the component unmounts
-  }, []);
-
   return (
     <>
-    <Head>
-        <meta name="description" content="Coqurate Box is a corporate and personalized gifting solution company that believes in the power of thoughtful gifts to build connections and inspire loyalty." />
-        <meta property="og:title" content="Coqurate Box" />
-        <meta property="og:description" content="Coqurate Box is a corporate and personalized gifting solution company that believes in the power of thoughtful gifts to build connections and inspire loyalty." />
-        <meta property="og:image" content="https://www.coquratebox.com/path-to-logo.png" />
-        <meta property="og:url" content="https://www.coquratebox.com" />
-        <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify({
-          "@context": "http://schema.org",
-          "@type": "Organization",
-          "url": "https://www.coquratebox.com",
-          "logo": "https://www.coquratebox.com/path-to-logo.png",
-          "name": "Coqurate Box",
-          "description": "Coqurate Box is a corporate and personalized gifting solution company.",
-          "sameAs": [
-            "https://www.instagram.com/coquratebox",
-            "https://www.linkedin.com/company/coquratebox",
-            "https://www.facebook.com/coquratebox"
-          ]
-        })}} />
-      </Head>
-    <main className="p-0 m-0 scroll-smooth">
-      <AnnouncementBar />
-      <Navbar />
-      <HeroSection />
-      <FeaturedCollection />
-      <CorporateBanner scrollToSection={scrollToSection}/>
-      <PersonalizeBanner scrollToSection={scrollToSection} />
-      <Testimonials />
-      <NotableClient />
-      <div ref={contactUsRef}>
-        <ContactUs />
-      </div>
-      <Footer />
-
-      {showPopup && <PopUp onClose={() => setShowPopup(false)}  scrollToSection={scrollToSection}/>}
-    </main>
+      <Pagecomponent />
     </>
   );
 };
